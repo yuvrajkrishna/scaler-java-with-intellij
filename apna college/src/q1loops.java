876d6fbd95ce61183facb2c7c7c330ceb037dd21
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-20 21:17:14 +0530

    loops q1 apna college

---
+++ apna college/src/q1loops.java
@@ -0,0 +1,10 @@
+public class q1loops {
+    static void main() {
+//        print numbers from 1 to 10;
+        int i = 1 ;
+        while(i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
