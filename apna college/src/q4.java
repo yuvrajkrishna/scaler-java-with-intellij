e7355829fe232c45b2467df267b3656ae5f7ec51
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-20 21:08:43 +0530

    q4 apna college

---
+++ apna college/src/q4.java
@@ -0,0 +1,9 @@
+public class q4 {
+    static void main() {
+        int a =  63 , b = 36;
+        boolean x = (a < b)? true:false;
+        int y = (a > b) ? a : b;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
