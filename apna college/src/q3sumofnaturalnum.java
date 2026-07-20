bb50e87a4d80f3073d9f648837d53c747aa01309
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-20 21:33:20 +0530

    q4 apc

--- apna college/src/q3sumofnaturalnum.java
+++ apna college/src/q3sumofnaturalnum.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class q3sumofnaturalnum {
+    static void main() {
+        System.out.println("Enter the number to get the sum till that number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        int i = 1 ;
+        while(i <= num){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
 }
---
+++ apna college/src/q4loops.java
@@ -0,0 +1,9 @@
+public class q4loops {
+    static void main() {
+        for(int i = 1 ; i < 5 ; i++){
+            System.out.println("Hello");
+            i+=2;
+        }
+    }
+}
+// it is supposed to run two times
