3fb62b64ea252663f1fd7d2da795fd252ac6e59e
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-23 21:12:35 +0530

    linearsearch apc

---
+++ apna college/src/reversearr.java
@@ -0,0 +1,17 @@
+public class reversearr {
+    static int linearSearch(int arr[],int key)
+    {
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    static void main() {
+        int key = 10;
+        int arr[] = {2,4,6,8,10};
+        int ans = linearSearch(arr,key);
+        System.out.println(ans);
+    }
+}
